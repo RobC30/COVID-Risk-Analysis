0dd8c3dd43e9c2e0187041ed21d03cacbe90f0f4
michaelxie1 <60826549+michaelxie1@users.noreply.github.com>
2022-01-11 11:36:49 -0500

    Added SQL File, no major change

---
+++ Resources/covid_19_SQL_Code.sql
@@ -0,0 +1,2 @@
+CREATE TABLE Covid19_Initial_Data();
+COPY Covid19_Initial_Data FROM 'C:\Users\Michael\OneDrive\Desktop\DataAnalyticsBootCamp\Final_Project\COVID-Risk-Analysis\Resources\initial_covid_data.csv' DELIMITER ' ';
