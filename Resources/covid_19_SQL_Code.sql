bc5449402fa7919df536acae601b0bcd007773d2
michaelxie1 <60826549+michaelxie1@users.noreply.github.com>
2022-01-12 18:24:34 -0500

    Updated SQL File (incomplete)

--- Resources/covid_19_SQL_Code.sql
+++ Resources/covid_19_SQL_Code.sql
@@ -1,2 +1,31 @@
 CREATE TABLE Covid19_Initial_Data();
 COPY Covid19_Initial_Data FROM 'C:\Users\Michael\OneDrive\Desktop\DataAnalyticsBootCamp\Final_Project\COVID-Risk-Analysis\Resources\initial_covid_data.csv' DELIMITER ' ';
+
+CREATE TABLE Covid_CLean_Data(
+	Row_ID,
+	continent,
+	locations,
+	dates,
+	new_cases,
+	new_deaths,
+	new_cases_smoothed_per_million,
+	reproduction_rate,
+	new_tests_smoothed,
+	tests_units,
+	stringency_index,
+	population,
+	population_density,
+	median_age,
+	aged_65_older,
+	aged_70_older,
+	gdp_per_capita,
+	extreme_poverty,
+	cardiovasc_death_rate,
+	diabetes_prevalence,
+	female_smokers,
+	male_smokers,
+	hospital_beds_per_thousand,
+	life_expectancy,
+	human_development_index
+);
+--COPY Covid19_Initial_Data FROM 'C:\Users\Michael\OneDrive\Desktop\DataAnalyticsBootCamp\Final_Project\COVID-Risk-Analysis\Resources\Covid_Cleaned_Database.csv' DELIMITER ' ';
